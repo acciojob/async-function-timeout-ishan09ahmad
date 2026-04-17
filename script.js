8e59d67936c4278e8cb9e1b54471714776b3fc9c
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-17 23:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let textEl=document.querySelector("#text");
+let delayEl=document.querySelector("#delay");
+let buttonEl=document.querySelector("#btn");
+let divEl=document.querySelector("#output");
+buttonEl.addEventListener("click",()=>
+	{
+		let text=textEl.value;
+		let delay=Number(delayEl.value);
+    showText(text,delay)
+	})
+
+async function showText(text,delay)
+{
+	let data=await new Promise((resolve,reject)=>
+		{
+			setTimeout(()=>
+				{
+        
+					resolve(text)
+				},delay)
+		})
+  divEl.textContent=data;
+}
